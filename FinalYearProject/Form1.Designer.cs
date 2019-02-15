d1ee8ed184222843d0c02f57238e65bea1d2124b
demi sho <demiscology@gmail.com>
2019-02-15 12:47:14 +0100

    Add project files.

---
+++ FinalYearProject/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace FinalYearProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.malariaDetectionDataSet = new FinalYearProject.MalariaDetectionDataSet();
+            this.imagesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.imagesTableAdapter = new FinalYearProject.MalariaDetectionDataSetTableAdapters.ImagesTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ImagePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malariaDetectionDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Malaria Detection System";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 756);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 716);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 104);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Azure;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Linen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(3, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(275, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Choose Image";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Linen;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(284, 653);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(271, 52);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Test Blood Samples";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Linen;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Bisque;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(280, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(275, 52);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Add Image";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // malariaDetectionDataSet
+            // 
+            this.malariaDetectionDataSet.DataSetName = "MalariaDetectionDataSet";
+            this.malariaDetectionDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // imagesBindingSource
+            // 
+            this.imagesBindingSource.DataMember = "Images";
+            this.imagesBindingSource.DataSource = this.malariaDetectionDataSet;
+            // 
+            // imagesTableAdapter
+            // 
+            this.imagesTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.ImagePath});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 397);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(552, 250);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Linen;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(3, 653);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(275, 52);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Clear Database";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ID.DataPropertyName = "Id";
+            this.ID.HeaderText = "Image ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // ImagePath
+            // 
+            this.ImagePath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ImagePath.DataPropertyName = "Image_Path";
+            this.ImagePath.HeaderText = "Image Path";
+            this.ImagePath.Name = "ImagePath";
+            this.ImagePath.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 850);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malariaDetectionDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private MalariaDetectionDataSet malariaDetectionDataSet;
+        private System.Windows.Forms.BindingSource imagesBindingSource;
+        private MalariaDetectionDataSetTableAdapters.ImagesTableAdapter imagesTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImagePath;
+    }
+}
+
---
+++ FinalYearProject/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using System.Data.SqlClient;
+
+namespace FinalYearProject
+{
+    public partial class Form1 : Form
+    {
+        string Connectionstring = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=MalariaDetection; Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Connectionstring))
+            {
+                sqlConnection.Open();
+                using (SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM dbo.Images", sqlConnection))
+                {
+
+                    DataTable data = new DataTable();
+                    sqlData.Fill(data);
+
+                    dataGridView1.DataSource = data;
+                }
+                sqlConnection.Close();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("http://127.0.0.1:8881/models/api_test/v2/predict");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Postman-Token", "53711025-2b90-4aae-820f-3fee76be3738");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"data\"\r\n\r\n{\"key\": \"Image\"};type=application/json\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Image\"; filename=\"C:\\Users\\Enoch Sodiya\\Desktop\\cell_images\\Parasitized\\Parasitized (11).png\"\r\nContent-Type: image/png\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            textBox1.Text = content;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ImageFileLocation = "";
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "All Files(*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ImageFileLocation = openFileDialog.FileName;
+
+                    pictureBox1.ImageLocation = ImageFileLocation;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox1.ImageLocation == null)
+            {
+                MessageBox.Show("Please Select an Image First", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(Connectionstring))
+                {
+                    sqlConnection.Open();
+
+                    using (SqlCommand sqlData = new SqlCommand("INSERT INTO Images (Image_Path) VALUES ('" + this.pictureBox1.ImageLocation + "')", sqlConnection))
+                    {
+                        sqlData.Connection = sqlConnection;
+                        sqlData.ExecuteNonQuery();
+
+                    }
+
+                    using (SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM dbo.Images", sqlConnection))
+                    {
+
+                        DataTable data = new DataTable();
+                        sqlData.Fill(data);
+
+                        dataGridView1.DataSource = data;
+                    }
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sql = new SqlConnection(Connectionstring))
+            {
+                sql.Open();
+
+                using (SqlCommand sqlData = new SqlCommand("DELETE FROM Images"))
+                {
+                    sqlData.Connection = sql;
+                    sqlData.ExecuteNonQuery();
+                }
+                using (SqlConnection sqlConnection = new SqlConnection(Connectionstring))
+                {
+                    sqlConnection.Open();
+                    using (SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM dbo.Images", sqlConnection))
+                    {
+
+                        DataTable data = new DataTable();
+                        sqlData.Fill(data);
+
+                        dataGridView1.DataSource = data;
+                    }
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] Imagepath = new string[dataGridView1.RowCount];
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                Imagepath[i] = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            }
+
+            for (int i = 0; i < Imagepath.Count(); i++)
+            {
+                var client = new RestClient("http://127.0.0.1:8881/models/api_test/v2/predict");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("cache-control", "no-cache");
+                //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+                request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"data\"\r\n\r\n{\"key\": \"Image\"};type=application/json\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"Image\"; filename=\"C:\\Users\\Enoch Sodiya\\Desktop\\cell_images\\Uninfected\\Uninfected (29).png\"\r\nContent-Type: image/png\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+
+                var content = response.Content;
+                textBox1.AppendText(content + " ");
+            }
+        }
+    }
+}
